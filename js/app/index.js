d71dbeae41c6ef8124baea38201d1ef6e9b1ecbd
Valentin Bragaru <valentin.bragaru@gmail.com>
2013-07-11 10:17:08 +0300

    Remove root path

---
+++ js/app/index.js
@@ -0,0 +1,31 @@
+﻿var myApp = angular.module('AngularChromeEx', []);
+
+myApp.config(function ($routeProvider){
+    $routeProvider
+    .when('/',{
+        controller:'HomeController',
+        templateUrl:chrome.extension.getURL('pages/home.html')
+    })
+    .when('/itemslist/:categoryurl',{
+        controller:'ItemsController',
+        templateUrl: chrome.extension.getURL('pages/itemsList.html')
+    }) 
+    .when('/iteminfo/:itemid',{
+        controller:'ItemInfoController',
+        templateUrl: chrome.extension.getURL('pages/itemInfo.html')
+    })
+    .when('/catinit',{
+        controller:'CatInitController',
+        templateUrl: chrome.extension.getURL('pages/catinit.html')
+    })
+    .when('/catbrowse/:parenturl',{
+        controller:'CatBrowseController',
+        templateUrl: chrome.extension.getURL('pages/catbrowse.html')
+    })
+    .otherwise({redirectTo:'/'})
+});
+
+
+
+
+
